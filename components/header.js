72c08be729b003cc3a4b91cc1c2662e30748939a
Austin Taylor <Austin@Austins-MBP.attlocal.net>
2021-09-10 13:44:51 -0500

    Code Cleanup

--- components/header.js
+++ components/header.js
@@ -36,10 +36,9 @@ class Header extends React.Component{
         this.setState({saying: saying});
     }
     render () {
-        const saying = this.state.saying;
         return (
             <div className={"flex-col flex p-10"}>
-                <code className={"bg-gray-50 p-2 max-w-sm mx-auto text-center"}>{saying}</code>
+                <code className={"bg-gray-50 p-2 max-w-sm mx-auto text-center"}>{this.state.saying}</code>
                 <p className={"pt-3 pb-3 text-white font-bold mx-auto text-5xl text-center"}>Welcome to my link-hub!</p>
                 <div className={"pt-10 pl-10 pr-10 max-w-sm mx-auto"}>
                     <Image className={"grayscale overflow-hidden rounded-full shadow-md hover:grayscale-0"}
