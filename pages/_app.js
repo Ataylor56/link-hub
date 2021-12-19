821344e38bee676128fc06856f57b5b5fda3b953
Austin Taylor <Austin@AMB0003.local>
2021-12-19 01:07:58 -0600

    Formatting Updates

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,20 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   const analyticsId = process.env.NEXT_PUBLIC_ANALYTICS_ID
   const source = `https://www.googletagmanager.com/gtag/js?id=${analyticsId}`
-  return <>
-
-    <Script id='GoogleAnalytics' strategy="lazyOnload" src={source} />
-    <Script id ='GoogleAnalytics' strategy="lazyOnload">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', ${analyticsId});
-      `}
-    </Script>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Script id='GoogleAnalytics' strategy="lazyOnload" src={source} />
+      <Script id='GoogleAnalytics' strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${analyticsId});
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
